c9d4609aebe6e5e6f2f0978e5db0ba9ed15846ce
Ti-Tai Wang <r04945002@ntu.edu.tw>
2017-02-03 05:14:34 +0000

    GSEA_update
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/anamiR@126424 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/kegg.gs.R
@@ -0,0 +1,10 @@
+#' KEGG pathways with gene set information
+#'
+#' This table is originally from \code{\link[gage]{gage}},
+#' including 177 pathways.
+#'
+#' @format A list with 177 KEGG pathways.
+#' @return list
+#' @source \code{\link[gage]{gage}}
+#' \url{https://bioconductor.org/packages/release/bioc/html/gage.html}
+"kegg.gs"
