66faa743433d09fdd790524c43a17670fba27170
Ti-Tai Wang <r04945002@ntu.edu.tw>
2017-04-23 01:15:04 +0000

    add_data_man
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/anamiR@129047 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/data_table_pre.R
@@ -0,0 +1,11 @@
+#' A list with information of genes and miRNAs.
+#'
+#' This table is generated from \link{miR_converter},
+#' including 4 files.
+#'
+#' @format A large list with 2 gene expression files
+#'  and 2 miRNAs files
+#' @return list
+#' @source NCBI GEO:
+#'  \url{http://www.ncbi.nlm.nih.gov/geo/}
+"table_pre"
